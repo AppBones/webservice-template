c01a65acfeef3198023ac5edb92210c951e4fa22
warreq <brandon.s.lucas@gmail.com>
2016-03-18 19:34:30 +0100

    remove some unnecessary callbacks from liberator
    swagger1st verifies that (json at least) is not malformed and
    returns the correct HTTP status, and it also returns the appropriate
    response when given an unsupported content-type.

--- src/appbone_service_template/api.clj
+++ src/appbone_service_template/api.clj
@@ -4,27 +4,6 @@
             [liberator.core :refer [resource]]
             [appbone-service-template.db :as db]))
 
-(defn parse-json [ctx key]
-  "When ctx has verb PUT or POST, try to parse body as json and store it in ctx."
-  (when (#{:put :post} (get-in ctx [:request :request-method]))
-    (try
-      (let [body (get-in ctx [:request :body])]
-        (let [data (json/read-str body)]
-          [false {key data}])
-        {:message "No body"})
-      (catch Exception e
-        (.printStackTrace e)
-        {:message (format "IOException: %s" (.getMessage e))}))))
-
-(defn check-content-type [ctx content-types]
-  "When ctx has verb PUT or POST, return true if the content-type is json."
-  (if (#{:put :post} (get-in ctx [:request :request-method]))
-    (or
-     (some #{(get-in ctx [:request :headers "content-type"])}
-           content-types)
-     [false {:message "Unsupported Content-Type"}])
-    true))
-
 (defn create-greeting
   "Generate a greeting message and return it."
   [request db]
@@ -44,8 +23,6 @@
         (resource
          :allowed-methods [:post :get]
          :available-media-types ["application/json"]
-         :known-content-type? (check-content-type req ["application/json"])
-         :malformed? (parse-json req :data)
          :post! (post-greeting req db)
          :handle-ok (create-greeting req db))]
     (handler req)))
